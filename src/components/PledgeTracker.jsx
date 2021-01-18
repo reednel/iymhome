fec8a43034d8f569e77f58e0e8953676487a3a73
cmdrury <61168979+cmdrury@users.noreply.github.com>
2021-01-18 16:45:24 -0600

    Add files via upload

--- src/components/PledgeTracker.jsx
+++ src/components/PledgeTracker.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
+import ProgressBar from 'react-bootstrap/ProgressBar'
 
 import "../css/Styles.css";
 
+const pledges = 40;
+
+
 class PledgeTracker extends Component {
   render() {
-    return <div></div>;
+    return (
+    <div classname="Pledge-Tracker">
+      <div classname="Pledge-Tracker-Filler">
+      <ProgressBar animated now={pledges} label ={pledges} variant = "danger" />;
+      </div>
+    </div>
+    )
   }
 }
 
